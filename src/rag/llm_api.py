b1970c38225178899c6eb2c227ede282718bba21
Sh1nroq <dorovskih2005@gmail.com>
2026-03-12 20:45:53 +0300

    fixed: CI errors

--- src/rag/llm_api.py
+++ src/rag/llm_api.py
@@ -2,21 +2,13 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 
-def get_answer_llm(message:str) -> str:
+def get_answer_llm(message: str) -> str:
     load_dotenv()
 
-    client = OpenAI(
-      base_url="https://openrouter.ai/api/v1",
-      max_retries=5
-    )
+    client = OpenAI(base_url="https://openrouter.ai/api/v1", max_retries=5)
 
     completion = client.chat.completions.create(
-      model="openai/gpt-oss-120b:free",
-      messages=[
-        {
-          "role": "user",
-          "content": message
-        }
-      ]
+        model="openai/gpt-oss-120b:free",
+        messages=[{"role": "user", "content": message}],
     )
     return completion.choices[0].message.content or ""
